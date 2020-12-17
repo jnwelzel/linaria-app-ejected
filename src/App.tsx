40b2ddf1cb70c4fce67cf8ba74feeeab30c77fa4
Jonathan Welzel <jnwelzel@gmail.com>
2020-12-17 16:45:25 +0100

    Linaria ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import { styled } from '@linaria/react'
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+    <Container>
+      <AppHeader>
+        <Img src={logo} alt="logo" />
+        <Paragraph>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
+        </Paragraph>
+        <Link
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
+          >
           Learn React
-        </a>
-      </header>
-    </div>
-  );
+        </Link>
+      </AppHeader>
+    </Container>
+  )
 }
 
-export default App;
+const Container = styled.div`
+  text-align: center;
+`
+
+const AppHeader = styled.header`
+  display: flex;
+  min-height: 100vh;
+  background-color: gray;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
+
+const Img = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+
+  @media (prefers-reduced-motion: no-preference) {
+    animation: App-logo-spin infinite 20s linear;
+  }
+
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`
+
+const Paragraph = styled.p``
+
+const Link = styled.a`
+  color: #61dafb;
+`
+
+export default App
